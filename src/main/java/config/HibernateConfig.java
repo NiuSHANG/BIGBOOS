cf0fb32aa79c94403314827e512a0cd5f0a810c5
Gresstant <gresstant@outlook.com>
2019-05-24 21:01:16 +0800

    change hibernate dialect to MySQL8Dialect

--- src/main/java/config/HibernateConfig.java
+++ src/main/java/config/HibernateConfig.java
@@ -44,7 +44,7 @@ public class HibernateConfig {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty("hibernate.hibernate.hbm2ddl.auto", "update");
         hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
         hibernateProperties.setProperty("hibernate.show_sql", "true");
 
         return hibernateProperties;
