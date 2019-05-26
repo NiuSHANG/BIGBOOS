64db7bc2f3346ad9323ed2ee625a01f2a007fe8d
Gresstant <gresstant@outlook.com>
2019-05-26 15:35:22 +0800

    generate methods

--- src/test/java/service/AdminServiceTest.java
+++ src/test/java/service/AdminServiceTest.java
@@ -24,44 +24,109 @@ public class AdminServiceTest {
     @Autowired
     private AdminService adminService;
 
-    private LocalDate date = LocalDate.of(2000, 1, 1);
     private Admin admin;
     private BookProfile profile;
 
     @Before
     public void setUp() {
         admin = adminService.addAdmin(Admin.builder().name("__test").password("123456").build());
-        profile = adminService.addBookProfile(new BookProfile(123456, "__test_profile", "gresstant", "type", date, 1.0, null));
-    }
-
-    @Test
-    public void test() {
-        Admin login = adminService.login("__test", "123456");
-        Assert.assertNotNull(login);
-        Assert.assertEquals("__test", login.getName());
-        Assert.assertEquals("123456", login.getPassword());
-
-        BookProfile bookProfile = adminService.findBookProfile(123456);
-        Assert.assertEquals(123456, bookProfile.getIsbn().intValue());
-        Assert.assertEquals("__test_profile", bookProfile.getName());
-        Assert.assertEquals("gresstant", bookProfile.getAuthor());
-        Assert.assertEquals("type", bookProfile.getType());
-        Assert.assertEquals(date, bookProfile.getIssueOn());
-        Assert.assertEquals(1.0, bookProfile.getPrice(), Double.MIN_VALUE);
-        Assert.assertNull(bookProfile.getCopies());
-
-        HashMap<String, Object> criteria = new HashMap<>();
-        criteria.put("isbn", 123456);
-        criteria.put("name", "__test_profile");
-        criteria.put("author", "gresstant");
-        List<BookProfile> bookByCriteria = adminService.findBookByCriteria(criteria);
-        Assert.assertEquals(1, bookByCriteria.size());
-        Assert.assertEquals(123456, bookByCriteria.get(0).getIsbn().intValue());
+        profile = adminService.addBookProfile(new BookProfile(123456, "__test_profile", "gresstant", "type", LocalDate.of(2000, 1, 1), 1.0, null));
     }
 
     @After
     public void tearDown() {
         adminService.removeAdmin(admin);
         adminService.removeBookProfile(profile);
+    }
+
+    @Test
+    public void findUser() {
+    }
+
+    @Test
+    public void findBookProfile() {
+        BookProfile bookProfile = adminService.findBookProfile(123456);
+        Assert.assertEquals(profile.getIsbn(), bookProfile.getIsbn());
+        Assert.assertEquals(profile.getName(), bookProfile.getName());
+        Assert.assertEquals(profile.getAuthor(), bookProfile.getAuthor());
+        Assert.assertEquals(profile.getType(), bookProfile.getType());
+        Assert.assertEquals(profile.getIssueOn(), bookProfile.getIssueOn());
+        Assert.assertEquals(profile.getPrice(), bookProfile.getPrice(), Double.MIN_VALUE);
+        Assert.assertNull(bookProfile.getCopies());
+    }
+
+    @Test
+    public void findBookCopy() {
+    }
+
+    @Test
+    public void findBookByCriteria() {
+        HashMap<String, Object> criteria = new HashMap<>();
+        criteria.put("isbn", profile.getIsbn());
+        criteria.put("name", profile.getName());
+        criteria.put("author", profile.getAuthor());
+        List<BookProfile> bookByCriteria = adminService.findBookByCriteria(criteria);
+        Assert.assertTrue(bookByCriteria.size() >= 1);
+        Assert.assertEquals(profile.getIsbn(), bookByCriteria.get(0).getIsbn());
+    }
+
+    @Test
+    public void findRecordByCriteria() {
+    }
+
+    @Test
+    public void addAdmin() {
+    }
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void addBookProfile() {
+    }
+
+    @Test
+    public void addBookCopy() {
+    }
+
+    @Test
+    public void removeAdmin() {
+    }
+
+    @Test
+    public void removeUser() {
+    }
+
+    @Test
+    public void removeBookProfile() {
+    }
+
+    @Test
+    public void removeBookCopy() {
+    }
+
+    @Test
+    public void updateAdmin() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void updateBookProfile() {
+    }
+
+    @Test
+    public void updateBookCopy() {
+    }
+
+    @Test
+    public void login() {
+        Admin login = adminService.login("__test", "123456");
+        Assert.assertNotNull(login);
+        Assert.assertEquals(admin.getName(), login.getName());
+        Assert.assertEquals(admin.getPassword(), login.getPassword());
     }
 }
