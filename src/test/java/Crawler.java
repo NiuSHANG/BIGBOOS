95d376f28807e2c31af93e40a23f8a732e8421e5
Gresstant <gresstant@outlook.com>
2019-06-01 22:21:05 +0800

    finished toBookProfile and (some part of) CLI

--- src/test/java/Crawler.java
+++ src/test/java/Crawler.java
@@ -6,12 +6,65 @@ import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 @Slf4j
 public class Crawler {
     public static void main(String[] args) throws IOException {
-        System.out.println(toBookProfile(25300595));
+        System.out.println("========================================");
+        System.out.println("       Crawler of Library Manager       ");
+        System.out.println("   as a component of the Final Handout  ");
+        System.out.println("========================================");
+        System.out.println();
+        System.out.println("Input a number or url to convert it to BookProfile and store it temporarily.");
+        System.out.println("Input \"show\" (without quotes) to list all stored BookProfiles.");
+        System.out.println("Input \"insert\" (without quotes) to add all stored BookProfiles to the database " +
+                                        "(will establish a connection if there isn't any available).");
+        System.out.println("Input \"exit\" (without quotes) to exit.");
+        System.out.println("========================================");
+        System.out.println("Please begin your performance. (๑•̀ㅂ•́)و✧");
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            if (!CLI.process(sc.nextLine()))
+                return;
+        }
+    }
+
+    private static class CLI {
+        private static List<BookProfile> books = new LinkedList<>();
+
+        public static boolean process(String input) {
+            try {
+                if (input == null) {
+                    System.out.println("`input` can't be null!");
+                } else if (input.equals("show")) {
+                    books.forEach(System.out::println);
+                } else if (input.equals("insert")) {
+                    System.out.println("Not implemented."); // TODO
+                } else if (input.equals("exit")) {
+                    System.out.println("Exiting...");
+                    return false; // terminate the program
+                } else {
+                    if (input.trim().matches("-?\\d+(\\.\\d+)?")) {
+                        System.out.println("Trying to add a BookProfile to temp list by id ...");
+                        books.add(toBookProfile(Integer.parseInt(input)));
+                    } else if (input.trim().matches("^https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")) {
+                        System.out.println("Trying to add a BookProfile to temp list by url ...");
+                        books.add(toBookProfile(input));
+                    } else {
+                        System.out.println("Doesn't understand.");
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            return true; // allow next input
+        }
     }
 
     private static BookProfile toBookProfile(int id) throws IOException {
